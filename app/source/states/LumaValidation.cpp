181795d4b24542e40d8ccdcf4b1c092021f758cc
itsglowny - toadysh <itsglowny@gmail.com>
2026-05-03 21:50:25 +0200

    Fix misleading indentation warning in LumaValidation.cpp

--- app/source/states/LumaValidation.cpp
+++ app/source/states/LumaValidation.cpp
@@ -119,7 +119,7 @@ bool LumaValidation::checkIfLumaOptionsEnabled(MainStruct *mainStruct, C3D_Rende
         if (kDown & KEY_A) return true;
     }
     // else if either external firms and modules or game patching is not enabled, send another error and draw luma info if b is pressed
-    else if (!mainStruct->externalFirmsAndModulesEnabled || !mainStruct->gamePatchingEnabled)
+    else if (!mainStruct->externalFirmsAndModulesEnabled || !mainStruct->gamePatchingEnabled) {
         if (kDown & KEY_B) {
             PlaySFX("romfs:/sfx/BIN_TRUE.wav");
             drawLumaInfo(mainStruct);
@@ -128,10 +128,11 @@ bool LumaValidation::checkIfLumaOptionsEnabled(MainStruct *mainStruct, C3D_Rende
             C2D_DrawSprite(&mainStruct->blank_info_message);
             DrawString(0.5f, infoColor, std::format("Enable external FIRMs and modules: {}\nEnable game patching: {}\n\nFor {} to work, both of these Luma3DS options should be ENABLED. To open Luma3DS settings, hold SELECT while booting your system.\n\nIf you are sure both options are enabled and the options shown don't match your Luma3DS settings, please go to our Discord and open a support forum with an image of the more information screen attached.\nPress A to exit, or hold B for more information.", mainStruct->externalFirmsAndModulesEnabled, mainStruct->gamePatchingEnabled, APP_TITLE), 0);
         }
-        
+
         // if A is pressed, return true to exit, else if X and Y is pressed
         if (kDown & KEY_A) return true;
         else if (kDown & KEY_X && kDown & KEY_Y) mainStruct->state = 1; // bypass if I need some time to fix it and get it released
+    }
     else {
         if (kDown & KEY_A) drawLumaInfo(mainStruct);
         else mainStruct->state = 1; // if A is held, show information, else go to the main menu
